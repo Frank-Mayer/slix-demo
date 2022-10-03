7434e90bf9dd36296757be15e9c3082991f2f44d
Frank-Mayer <mail@frank-mayer.io>
2022-10-04 01:16:46 +0200

    removed not needed react

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import React from "react";
 import { slix, resetCss, ArrowController } from "@frank-mayer/slix";
 import { Slide01 } from "./slides/Slide01";
 import { Slide02 } from "./slides/Slide02";
@@ -7,10 +6,10 @@ import { Slide04 } from "./slides/Slide04";
 
 slix({
   slides: new Map([
-    [1, <Slide01 />],
-    [2, <Slide02 />],
-    [3, <Slide03 />],
-    [4, <Slide04 />],
+    [1, Slide01()],
+    [2, Slide02()],
+    [3, Slide03()],
+    [4, Slide04()],
   ]),
   initialSlide: 1,
 }).then((slixInstance) => {
